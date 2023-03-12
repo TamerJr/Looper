50bb1cb528dfd3704652e333ef372bada584f02d
TamerJr <ahmttmer@gmail.com>
2023-03-12 19:36:13 +0200

    responsive navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
-import React,{useContext} from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import Looper from "../../Assets/Looper.png";
 import userContext from "../../Utiles/Context/AuthContext";
 import "./Navbar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Navbar = () => {
-  const {handleLogOut,user}=useContext(userContext)
+  const { handleLogOut, user } = useContext(userContext);
+  const [toggled,setToggled] =useState(false)
   return (
     <nav>
       <Link to="/">
-      <img src={Looper} alt="Logo" title="Looper" />
+        <img src={Looper} alt="Logo" title="Looper" />
       </Link>
       {/* nav menu */}
-      <ul>
+      <ul className="navigation">
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -24,26 +26,67 @@ const Navbar = () => {
         <li>
           <Link to="/shop">Shop</Link>
         </li>
-         {user&&
+        {user && (
           <li>
-           <Link to="/account">Account</Link>
+            <Link to="/account">Account</Link>
           </li>
-         }
+        )}
       </ul>
       {/* Auth section */}
-      <ul>
-        {!user?
+      <ul className="navigation AccountButton">
+        {!user ? (
           <li>
             <Link to="/login">Log In</Link>
-          </li>:
-          <li>
-            <button onClick={()=>handleLogOut()}>Log Out</button>
           </li>
-        }
+        ) : (
+          <li>
+            <button onClick={() => handleLogOut()}>Log Out</button>
+          </li>
+        )}
         <li>
           <Link to="/signup">Sign Up</Link>
         </li>
       </ul>
+      <section className="ToggledMenu">
+        <RxHamburgerMenu size={25} className="Hamburger" onClick={()=>setToggled(!toggled)}/>
+        <div className={toggled?"toggled toggleClick" :"toggled"}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/terms">Terms</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            {user && (
+              <li>
+                <Link to="/account">Account</Link>
+              </li>
+            )}
+          </ul>
+          <div>
+            <ul className="toggled-menu">
+              {!user ? (
+                <li>
+                  <Link to="/login">Log In</Link>
+                </li>
+              ) : (
+                <li>
+                  <button onClick={() => handleLogOut()} className="logOutMenu">Log Out</button>
+                </li>
+              )}
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </nav>
   );
 };
